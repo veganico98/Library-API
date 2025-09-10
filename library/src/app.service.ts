24099a79200a65d59005552137a8f2e3984b636f
Nicolas <nicolaslinaresoli@gmail.com>
2025-09-10 17:37:17 -0300

    Correção de postman collection

--- library/src/app.service.ts
+++ library/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Biblioteca-api-Nicolas. Escolha um livro, empreste ou devolva. Faça silêncio!';
   }
 }
